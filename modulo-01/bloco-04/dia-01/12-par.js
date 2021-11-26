1d1b08a1f273e2841a6c73bfd604cc9be9db58a2
Gudine <gudine283@gmail.com>
2021-11-26 18:46:06 -0300

    Cria programas para comparar (im)paridade dos números

---
+++ modulo-01/bloco-04/dia-01/12-par.js
@@ -0,0 +1,9 @@
+const a = 9;
+const b = 12;
+const c = -3;
+
+if (a%2 == 0 || b%2 == 0 || c%2 == 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
---
+++ modulo-01/bloco-04/dia-01/13-impar.js
@@ -0,0 +1,9 @@
+const a = 9;
+const b = 12;
+const c = -3;
+
+if (a%2 == 1 || b%2 == 1 || c%2 == 1) {
+  console.log(true);
+} else {
+  console.log(false);
+}
