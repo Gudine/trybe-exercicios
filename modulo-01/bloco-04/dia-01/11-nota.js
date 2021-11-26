eb2dc0bc73a3082b22d112a9908ffbbdfc1e2951
Gudine <gudine283@gmail.com>
2021-11-26 18:43:18 -0300

    Cria programa para transformar notas em letras

---
+++ modulo-01/bloco-04/dia-01/11-nota.js
@@ -0,0 +1,10 @@
+const nota = 95;
+
+if (nota > 100) {console.log("ERRO: Nota maior que 100!")}
+else if (nota >= 90) {console.log("A")}
+else if (nota >= 80) {console.log("B")}
+else if (nota >= 70) {console.log("C")}
+else if (nota >= 60) {console.log("D")}
+else if (nota >= 50) {console.log("E")}
+else if (nota >= 0) {console.log("F")}
+else {console.log("ERRO: Nota menor que 0!")}
