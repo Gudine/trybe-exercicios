5daed79c7efd60a0572ea70b41ecfa60b1b20bfd
Gudine <gudine283@gmail.com>
2021-11-29 15:29:28 -0300

    Cria novo script.js, para realizar exercícios relacionados aos arrays

---
+++ modulo-01/bloco-04/dia-02/fixação/script.js
@@ -0,0 +1,15 @@
+//Exercício 1
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+//Exercício 2
+let indexOfPortfolio = menu.indexOf("Portfólio");
+
+console.log(indexOfPortfolio);
+
+//Exercício 3
+menu.push("Contato");
+
+console.log(menu);
