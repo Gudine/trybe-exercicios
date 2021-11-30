dc745f2ea928a48cabf275696c95b287ec0f8d1b
Gudine <gudine283@gmail.com>
2021-11-30 19:08:57 -0300

    Realiza exercícios bônus

--- modulo-01/bloco-04/dia-03/script.js
+++ modulo-01/bloco-04/dia-03/script.js
@@ -1,4 +1,4 @@
-const n = 9;
+const n = 29;
 
 // 1. Quadrado
 for (let i = 0; i < n; i += 1) {
@@ -32,7 +32,7 @@ for (let i = 0; i < n; i += 1) {
 
 console.log("");
 // 4. Pirâmide
-let rows = Math.ceil(n/2)
+let rows = Math.ceil(n/2);
 
 for (let i = 0; i < rows; i += 1) {
   let line = "";
@@ -43,4 +43,46 @@ for (let i = 0; i < rows; i += 1) {
     else {line += "*"};
   }
   console.log(line);
+}
+
+console.log("");
+// Bônus 5. Pirâmide Vazia
+for (let i = 0; i < rows; i += 1) {
+  let line = "";
+  let spaces = (rows - 1) - i;
+
+  for (let j = 0; j < n - spaces; j += 1) {
+    if (j == spaces || j == (n - spaces - 1)) {
+      line += "*";
+    } else if (i == rows - 1) {
+      line += "*";
+    } else {
+      line += " ";
+    }
+  }
+  console.log(line);
+}
+
+console.log("");
+// Bônus 6. Verificação de Número Primo
+
+let primes = [];
+
+for (let i = 2; i <= n; i += 1) {
+  let isPrime = true;
+  for (let x of primes) {
+    if (i%x == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  if (isPrime) {
+    primes.push(i);
+  }
+}
+
+if (primes.splice(-1)[0] == n) {
+  console.log("Número é primo!")
+} else {
+  console.log("Número não é primo!")
 }
