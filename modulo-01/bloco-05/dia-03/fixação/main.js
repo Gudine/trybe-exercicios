d5fb235b84680b6c031a18eaf2c43db87345d224
Gudine <gudine283@gmail.com>
2021-12-07 15:15:03 -0300

    Realiza requisitos 1 e 2

--- modulo-01/bloco-05/dia-03/fixação/main.js
+++ modulo-01/bloco-05/dia-03/fixação/main.js
@@ -8,10 +8,22 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+// Resposta: Porque tem um translateY na classe tech
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+firstLi.addEventListener('click', techElevate);
+secondLi.addEventListener('click', techElevate);
+thirdLi.addEventListener('click', techElevate);
 
+function techElevate(event) {
+  let liList = [firstLi, secondLi, thirdLi];
+  for (let x of liList) {
+    x.classList.remove("tech");
+  }
+
+  event.target.classList.add("tech");
+}
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
