40035c7698d4b911e9d945c79d01fc3d828c56c8
Gudine <gudine283@gmail.com>
2021-12-07 15:30:05 -0300

    Realiza requisito 5

--- modulo-01/bloco-05/dia-03/fixação/main.js
+++ modulo-01/bloco-05/dia-03/fixação/main.js
@@ -45,6 +45,16 @@ function goToPortfolio() {
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+myWebpage.addEventListener('mouseover', headerColorOn);
+myWebpage.addEventListener('mouseout', headerColorOff);
+
+function headerColorOn(event) {
+  event.target.style.color = "lightgreen";
+}
+
+function headerColorOff(event) {
+  event.target.style.color = null;
+}
 
 // Segue abaixo um exemplo do uso de event.target:
 
