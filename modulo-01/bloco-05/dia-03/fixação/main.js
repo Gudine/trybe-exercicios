e5ea488e547d3b8e7e92229e862313c229d3091b
Gudine <gudine283@gmail.com>
2021-12-07 15:23:15 -0300

    Realiza requisito 4

--- modulo-01/bloco-05/dia-03/fixação/main.js
+++ modulo-01/bloco-05/dia-03/fixação/main.js
@@ -34,10 +34,14 @@ function changeTech(event) {
   tech.innerText = event.target.value;
 }
 
-
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+myWebpage.addEventListener('dblclick', goToPortfolio);
+
+function goToPortfolio() {
+  window.location.href = "https://gudine.github.io/portfolio-web/";
+}
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
