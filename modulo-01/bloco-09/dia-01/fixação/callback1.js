371134fbb15712f5488a764564e8ea9902d30633
Gudine <gudine283@gmail.com>
2022-01-28 15:37:51 -0300

    Realiza exercícios de fixação

---
+++ modulo-01/bloco-09/dia-01/fixação/callback1.js
@@ -0,0 +1,14 @@
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = (callback) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  return callback(userToReturn);
+};
+
+console.log(getUser(userFullName));
+console.log(getUser(userNationality));
---
+++ modulo-01/bloco-09/dia-01/fixação/callback2.js
@@ -0,0 +1,19 @@
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUser = (callback) => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian",
+    };
+    console.log(callback(user));
+    return callback
+  }, delay());
+};
+
+getUser(userFullName);
+getUser(userNationality);
