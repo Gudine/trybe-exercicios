36e1a23e881048edc878eb01e14325efea360883
Gudine <gudine283@gmail.com>
2022-01-31 22:15:13 -0300

    Adiciona conversão para Real

--- modulo-01/bloco-09/dia-02/api.js
+++ modulo-01/bloco-09/dia-02/api.js
@@ -1,7 +1,9 @@
 const ol = document.getElementById('list');
+let dollarToReal;
 
 const processPrice = (price) => {
-  const [dollars, cents] = parseFloat(price).toFixed(2).split('.');
+  const [dollars, cents] = parseFloat(price * dollarToReal).toFixed(2).split('.');
+  
   // Fonte: https://stackoverflow.com/a/47644519
   return dollars.match(/\d{1,3}(?=(\d{3})*$)/g).join('.') + ',' + cents;
 };
@@ -23,13 +25,16 @@ const displaySingleCoin = (coin) => {
   
   const symbolDiv = createElemShort('div', 'symbol', symbol, li);
   const nameDiv = createElemShort('div', 'name', name, container);
-  const priceDiv = createElemShort('div', 'price', `\$${price}`, container);
+  const priceDiv = createElemShort('div', 'price', `R\$${price}`, container);
 
   li.appendChild(container);
   ol.appendChild(li);
 }
 
 const displayFirstCoins = async () => {
+  dollarToReal = await fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json')
+    .then((response) => response.json())
+    .then((response) => response.usd.brl)
   const list = await fetch('https://api.coincap.io/v2/assets')
     .then((response) => response.json())
     .then((response) => response.data.slice(0,10));
