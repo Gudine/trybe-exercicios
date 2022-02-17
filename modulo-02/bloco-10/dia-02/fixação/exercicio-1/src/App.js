ccdfbe5fd4a01e3986e056e7e84cb2fcf58e9f45
Gudine <gudine283@gmail.com>
2022-02-17 15:15:03 -0300

    Realiza 1o exercício de fixação

---
+++ modulo-02/bloco-10/dia-02/fixação/exercicio-1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Image from './Image'
+
+function App() {
+  return (
+    <div className="App">
+      <section className="Exercise-1">
+        <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring"/>
+      </section>
+    </div>
+  );
+}
+
+export default App;
