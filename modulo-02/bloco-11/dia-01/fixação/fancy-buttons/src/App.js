eed7386ac40eb7c7c11cfb10561ea9c295f1047a
Gudine <gudine283@gmail.com>
2022-02-21 16:03:58 -0300

    Realiza exercício de fixação sobre estados e eventos

--- modulo-02/bloco-11/dia-01/fixação/fancy-buttons/src/App.js
+++ modulo-02/bloco-11/dia-01/fixação/fancy-buttons/src/App.js
@@ -8,24 +8,35 @@ const functionThree = (ev) => console.log('Funcionou mais uma vez!!!'); */
 class App extends Component {
   constructor() {
     super()
+
+    this.state = { btnOne: 0, btnTwo: 0, btnThree: 0 };
+
     this.functionOne = this.functionOne.bind(this);
     this.functionTwo = this.functionTwo.bind(this);
     this.functionThree = this.functionThree.bind(this);
   }
 
-  functionOne() { console.log('Funcionou!!!', this); }
-  functionTwo() { console.log('Funcionou de novo!!!', this); }
-  functionThree() { console.log('Funcionou mais uma vez!!!', this); }
+  functionOne() {
+    this.setState((prev, _props) => ({ btnOne: prev.btnOne + 1 }))
+  }
+  functionTwo() {
+    this.setState((prev, _props) => ({ btnTwo: prev.btnTwo + 1 }))
+  }
+  functionThree() {
+    this.setState((prev, _props) => ({ btnThree: prev.btnThree + 1 }))
+  }
 
-  render() { 
+  render() {
+    const { btnOne, btnTwo, btnThree } = this.state;
+
     return (
       <div className="App">
 {/*         <button onClick={functionOne}>Botão 1</button>
         <button onClick={functionTwo}>Botão 2</button>
         <button onClick={functionThree}>Botão 3</button> */}
-        <button onClick={this.functionOne}>Botão 1</button>
-        <button onClick={this.functionTwo}>Botão 2</button>
-        <button onClick={this.functionThree}>Botão 3</button>
+        <button onClick={this.functionOne}>Botão 1: {btnOne} cliques</button>
+        <button onClick={this.functionTwo}>Botão 2: {btnTwo} cliques</button>
+        <button onClick={this.functionThree}>Botão 3: {btnThree} cliques</button>
       </div>);
   }
 }
