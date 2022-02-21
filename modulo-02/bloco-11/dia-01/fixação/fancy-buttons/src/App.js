58e9294f136fe1edaa4bd1b6cf2161c834b07319
Gudine <gudine283@gmail.com>
2022-02-21 16:23:40 -0300

    Realiza exercício de fixação final

--- modulo-02/bloco-11/dia-01/fixação/fancy-buttons/src/App.js
+++ modulo-02/bloco-11/dia-01/fixação/fancy-buttons/src/App.js
@@ -9,14 +9,31 @@ class App extends Component {
   constructor() {
     super()
 
-    this.state = { btnOne: 0, btnTwo: 0, btnThree: 0 };
+    this.state = {
+      btnOne: { count: 0, color: 'green' },
+      btnTwo: { count: 0, color: 'green' },
+      btnThree: { count: 0, color: 'green' },
+    };
 
-    this.functionOne = this.functionOne.bind(this);
+    this.changeBtn = this.changeBtn.bind(this);
+/*     this.functionOne = this.functionOne.bind(this);
     this.functionTwo = this.functionTwo.bind(this);
-    this.functionThree = this.functionThree.bind(this);
+    this.functionThree = this.functionThree.bind(this); */
   }
 
-  functionOne() {
+  changeBtn(btn) {
+    this.setState((prev, _props) => {
+      const count = prev[btn].count + 1;
+      const result = {
+        count: count,
+        color: (count%2) ? null : 'green',
+      };
+      console.log(result.color);
+      return { [btn]: result };
+    });
+  }
+
+/*   functionOne() {
     this.setState((prev, _props) => ({ btnOne: prev.btnOne + 1 }))
   }
   functionTwo() {
@@ -24,7 +41,7 @@ class App extends Component {
   }
   functionThree() {
     this.setState((prev, _props) => ({ btnThree: prev.btnThree + 1 }))
-  }
+  } */
 
   render() {
     const { btnOne, btnTwo, btnThree } = this.state;
@@ -33,10 +50,28 @@ class App extends Component {
       <div className="App">
 {/*         <button onClick={functionOne}>Botão 1</button>
         <button onClick={functionTwo}>Botão 2</button>
-        <button onClick={functionThree}>Botão 3</button> */}
+        <button onClick={functionThree}>Botão 3</button>
         <button onClick={this.functionOne}>Botão 1: {btnOne} cliques</button>
         <button onClick={this.functionTwo}>Botão 2: {btnTwo} cliques</button>
-        <button onClick={this.functionThree}>Botão 3: {btnThree} cliques</button>
+        <button onClick={this.functionThree}>Botão 3: {btnThree} cliques</button> */}
+        <button
+          onClick={() => this.changeBtn('btnOne')}
+          style={{ backgroundColor: btnOne.color }}
+          >
+          Botão 1: {btnOne.count} cliques
+        </button>
+        <button
+          onClick={() => this.changeBtn('btnTwo')}
+          style={{ backgroundColor: btnTwo.color }}
+          >
+          Botão 2: {btnTwo.count} cliques
+        </button>
+        <button
+          onClick={() => this.changeBtn('btnThree')}
+          style={{ backgroundColor: btnThree.color }}
+          >
+          Botão 3: {btnThree.count} cliques
+        </button>
       </div>);
   }
 }
