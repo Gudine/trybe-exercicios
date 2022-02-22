03a238fc2131a7c55a08eb3687da8a7f7fd7ae4c
Gudine <gudine283@gmail.com>
2022-02-22 15:49:51 -0300

    Realiza exercício de fixação sobre forms e event handlers genéricos

--- modulo-02/bloco-11/dia-02/fixação/exercicio-forms/src/Form.jsx
+++ modulo-02/bloco-11/dia-02/fixação/exercicio-forms/src/Form.jsx
@@ -6,40 +6,81 @@ class Form extends Component {
 
     this.state = {
       username: '',
+      password: '',
+      tribe: 'A',
+      feedback: '',
+      agreement: false,
     };
 
-    this.handleUsernameInput = this.handleUsernameInput.bind(this);
+    this.handleInput = this.handleInput.bind(this);
   }
 
-  handleUsernameInput(ev) {
-    this.setState({
-      username: ev.target.value,
-    });
+  handleInput({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({[name]: value});
   }
 
   render() { 
     return (
       <form>
         <h1>Forms de feedback</h1>
+        <fieldset>
+          <legend>Identificação</legend>
+          <label>
+            Nome de usuário:
+            <input
+              name='username'
+              type='text'
+              value={this.state.username}
+              onChange={this.handleInput}
+            />
+          </label>
+          <label>
+            Senha:
+            <input
+              name='password'
+              type='password'
+              value={this.state.password}
+              onChange={this.handleInput}
+            />
+          </label>
+          <label>
+            Tribo:
+            <select
+              name='tribe'
+              value={this.state.tribe}
+              onChange={this.handleInput}
+            >
+              <option>A</option>
+              <option>B</option>
+              <option>C</option>
+            </select>
+          </label>
+        </fieldset>
+        <fieldset>
+          <legend>Informações a enviar</legend>
+          <label>
+            Feedback:
+            <textarea
+              name='feedback'
+              value={this.state.feedback}
+              onChange={this.handleInput}
+            />
+          </label>
+          <label>
+            Arquivo:
+            <input name='file' type="file" />
+          </label>
+        </fieldset>
         <label>
-          Nome de usuário:
-          <input name='username' type='text' onChange={this.handleUsernameInput}/>
-        </label>
-        <label>
-          Senha:
-          <input name='password' type='password' />
-        </label>
-        <label>
-          Tribo:
-          <select name='tribe'>
-            <option>A</option>
-            <option>B</option>
-            <option>C</option>
-          </select>
-        </label>
-        <label>
-          Feedback:
-          <textarea name='feedback' />
+          Eu aceito que meu feedback será lido pela equipe
+          <input
+            name='agreement'
+            type='checkbox'
+            checked={this.state.agreement}
+            onChange={this.handleInput}
+          />
         </label>
       </form>
     );
